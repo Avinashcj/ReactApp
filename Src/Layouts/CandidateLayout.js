f4a2242d03ded9aead6b00c3e1c04d424c39bf26
gohk007 <gohk007@gmail.com>
2021-05-02 17:42:22 +0530

    deleted files

--- Src/Layouts/CandidateLayout.js
+++ Src/Layouts/CandidateLayout.js
@@ -5,14 +5,8 @@ import { View, Text, Alert, Button, TextInput, TouchableOpacity } from 'react-na
 // import { Button, Icon, Menu } from "antd";
 import logo from '../../assets/tnsdc-logo1.jpg';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-   class CandidateLayout extends Component {
-
-
-    
-
+class CandidateLayout extends Component {
 render() {
-
-  
 const value = AsyncStorage.getItem('@storage_Key');
 return (
 
@@ -20,15 +14,10 @@ return (
         // <Header className="app-header">
           <div className="app-header__logo-search-section">
             <div className="app-header__logo">
-
             <img src={logo} alt="App logo" />
-<View>
+            <View>
             <Text  >Welcome {this.props.FirstName}</Text> 
-            {/* <Text>{value}</Text> */}
             </View>
-             {/* <Link to="/">
-                <img src={logo} alt="App logo" />
-              </Link>  */}
             </div>
           </div>
         // </Header>
