37c4e1e2f3c41d535b41f839d3ee3e228005c8f3
Avinashcj <avi.buddha@gmail.com>
2021-05-14 16:36:57 +0530

    Final
    Latest changes
    
    Co-Authored-By: Avinash Meetoo <avinashmeetoo@gmail.com>

--- Src/Layouts/CandidateLayout.js
+++ Src/Layouts/CandidateLayout.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import { View, Text, Alert, Button, TextInput, TouchableOpacity } from 'react-native';
 import logo from '../../assets/tnsdc-logo1.jpg';
@@ -56,53 +55,3 @@ function App1() {
   }
   }
   export default CandidateLayout;
-=======
-import * as React from 'react';
-import { Text, View ,Button, Alert} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-     <View>
-            <Text  >Welcome</Text>
-            <Button onPress={AsyncStorage.clear()}>
-              <Text>Clear Async Storage</Text>
-            </Button>
-    </View>
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Logout" component={SettingsScreen}  />
-    </Tab.Navigator>
-  );
-}
-
-export default function CandidateLayout() {
-  return (
-    <NavigationContainer>
-      <MyTabs  value={this.props.FirstName} />
-    </NavigationContainer>
-  );
-}
->>>>>>> b3ffd420c7c46121ab5cae9a889895ab02c91062
